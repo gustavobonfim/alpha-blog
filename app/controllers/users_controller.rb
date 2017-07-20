ed075e41b0034473438844bedde64aea61dad171
gustavobonfim <bonfimoliveira@gmail.com>
2017-07-20 18:46:48 -0300

    add restritions from UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
 
- def index
-   @users = User.paginate(page: params[:page], per_page: 5)
- end
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
 
 
 
@@ -25,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Account edited successfully"
       redirect_to articles_path
@@ -39,9 +38,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-
   end
 
 
@@ -49,6 +46,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
 
+  def set_user
+    @user = User.find(params[:id])
   end
 end
